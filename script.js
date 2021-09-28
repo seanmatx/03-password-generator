5dafb300f1604d7b071227303f6a52235ddedec7
seanmatx <35512562+seanmatx@users.noreply.github.com>
2021-09-28 13:37:41 -0700

    randomly generate characters for password

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-console.log (generateBtn);
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var symbols = " !@#$%^&*()_-+=";
+
+var passwordUpperCase;
+var passwordLowerCase;
+var passwordNumbers;
+var passwordSymbols;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,13 +18,49 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 function generatePassword(){
+  var password = "";
   var passwordLength = prompt("length of new password");
   console.log (passwordLength);
-  if (passwordLength < 8 || passwordLength > 128){
-    alert ("Password must be between 8 and 128 characters long.")
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Please enter a number between 8 - 128 indicating the length of your password")
+    return;
   }
-  // return "hello";
+  passwordUpperCase=confirm ("Do you want any uppercase characters in your password?");
+  passwordLowerCase=confirm ("Do you want any lowercase characters in your password?");
+  passwordNumbers=confirm ("Do you want any numbers in your password?");
+  passwordSymbols=confirm ("Would you like any symbols in your password?");
+
+  let possiblePassword = "";
+  if (passwordUpperCase){
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    possiblePassword+= upperCase;
+
+  }
+    if (passwordLowerCase){
+      password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+      possiblePassword+= lowerCase;
+    }
+      if (passwordNumbers){
+        password+=numbers[Math.floor(Math.random()*numbers.length)];
+        possiblePassword+= numbers;
+    }
+    if (passwordSymbols){
+      password+=symbols[Math.floor(Math.random()*symbols.length)];
+      possiblePassword+= symbols;
+  }
+    // taking true statement of uppercase, and selecting an uppercase character to add to password
+  
+
+  console.log (password);
+  console.log (possiblePassword);
+
+  for(var i=password.length; i<passwordLength; i++){
+    password+=possiblePassword[Math.floor(Math.random()*possiblePassword.length)];
+
+  }
+  return password;
 }
 
 // Add event listener to generate button
